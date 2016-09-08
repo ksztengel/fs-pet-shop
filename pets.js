6f5bb06b0fddc11872a8018543161330fe20c772
Kristin Sztengel <kcsztengel@gmail.com>
2016-09-07 18:37:01 -0600

    read and create.  working on read index and addin all 3 keys and value.currently adds 1 value

---
+++ pets.js
@@ -0,0 +1,49 @@
+var fs = require ('fs')
+var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
+
+var node = path.basename(process.argv[0]);
+var file = path.basename(process.argv[1]);
+var cmd = process.argv[2];
+
+if (cmd === 'read') {
+  fs.readFile(petsPath, 'utf8', function(err, data) {
+    if (err) {
+      throw err;
+    }
+
+    var pets = JSON.parse(data);
+
+    console.log(pets);
+  });
+}
+else if (cmd === 'create') {
+  fs.readFile(petsPath, 'utf8', function(readErr, data) {
+    if (readErr) {
+      throw readErr;
+    }
+
+    var pets = JSON.parse(data);
+    var pet = process.argv[3];
+
+    if (!pet) {
+      console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`);
+      process.exit(1);
+    }
+
+    pets.push(pet);
+
+    var petsJSON = JSON.stringify(pets);
+
+    fs.writeFile(petsPath, petsJSON, function(writeErr) {
+      if (writeErr) {
+        throw writeErr;
+      }
+      console.log(pet);
+    });
+  });
+}
+else {
+  console.error(`Usage: ${node} ${file} [read | create]`);
+  process.exit(1);
+}
