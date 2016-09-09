fee014675faea6f06463d4a5db7bc76dc79f7fac
Kristin Sztengel <kcsztengel@gmail.com>
2016-09-09 12:59:20 -0600

    working

--- server.js
+++ server.js
@@ -4,41 +4,42 @@ const path = require('path')
 const petsPath = path.join(__dirname, 'pets.json')
 const express = require('express')
 const app = express()
-// const env = require('dotenv').config()
+    // const env = require('dotenv').config()
 const port = process.env.PORT || 8000
 
-// app.get('/',function(req,res){
-//   res.send("Hello World")
-// })
 
-app.get('/pets', function (req,res){
-  fs.readFile(petsPath, 'utf8', function(err, petsJSON){
-    if (err){
-      console.error('err', err.stack);
-      return res.sendStatus(500)
-  }
-    let pets = JSON.parse(petsJSON)
-    res.send(pets)
-  })
+app.get('/pets', function(req, res) {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+        if (err) {
+            console.error('err', err.stack);
+            return res.sendStatus(500)
+        }
+        let pets = JSON.parse(petsJSON)
+        res.send(pets)
+    })
 
 })
 
-app.get('/pets/:index', function(req, res) {
-  fs.readFile(petsPath, 'utf8', function(err, petsJSON){
-    if (err) {
-      console.error('err', err.stack);
-      return res.sendStatus(500);
-    }
-    let index = Number.parseInt(req.params.index)
-    console.log('req.params', req.params);
-    let pets = JSON.parse(petsJSON)
-    if (index < 0 || index >= pets.length || Number.isNaN(index)){
-      res.sendStatus(404)
-    }
-    if (index === pets.index)
-    res.send(pets)
+app.get('/pets/:id', function(req, res) {
+
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+        if (err) {
+            console.error('err', err.stack);
+            return res.sendStatus(500);
+        }
+
+        console.log('req.params', req.params);
+        let id = Number.parseInt(req.params.id)
+        let pets = JSON.parse(petsJSON)
+
+        if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+            res.sendStatus(404)
+        }
+
+          res.send(pets[id])
+
+    })
 })
-})
-app.listen(port,function(){
-  console.log("Listening on port:", port);
+app.listen(port, function() {
+    console.log("Listening on port:", port);
 })
