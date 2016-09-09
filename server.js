27a95c678d50d3679d39475a57146c95bbdad55b
Kristin Sztengel <kcsztengel@gmail.com>
2016-09-09 09:20:29 -0600

    GET all of the array. working on the index

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
-const http = require('http');
+'use strict'
+const fs = require('fs')
+const path = require('path')
+const petsPath = path.join(__dirname, 'pets.json')
+const express = require('express')
+const app = express()
+// const env = require('dotenv').config()
+const port = process.env.PORT || 8000
 
-const hostname = '127.0.0.1';
-const port = 5000;
+// app.get('/',function(req,res){
+//   res.send("Hello World")
+// })
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.get('/pets', function (req,res){
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON){
+    if (err){
+      console.error('err', err.stack);
+      return res.sendStatus(500)
+  }
+    let pets = JSON.parse(petsJSON)
+    res.send(pets)
+  })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+})
+
+app.get('/pets/:index', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON){
+    if (err) {
+      console.error('err', err.stack);
+      return res.sendStatus(500);
+    }
+    let index = Number.parseInt(req.params.index)
+    console.log('req.params', req.params);
+    let pets = JSON.parse(petsJSON)
+    if (index < 0 || index >= pets.length || Number.isNaN(index)){
+      res.sendStatus(404)
+    }
+    if (index === pets.index)
+    res.send(pets)
+})
+})
+app.listen(port,function(){
+  console.log("Listening on port:", port);
+})
